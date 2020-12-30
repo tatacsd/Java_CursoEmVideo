e432d4c4fa183798ec58e9caf88456854bfad60b
Thays <thayscasado@hotmail.com>
2020-12-30 01:42:50 -0800

    Aula 4 – Primeiro Programa em Java

---
+++ PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+
+package primeiroprograma;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Olá, mundo");
+        System.out.println("Olá, mundo atalho");
+    }
+    
+}
